041dab4d57882a07e70c117318f79e2a8a02cb23
Joao Ponte <joao_ponte@msn.com>
2024-06-18 20:35:04 +0100

    Update commentcard, refactor CommentList to receive the comments. Create CommentSection.

--- nc-news/src/Components/CommentList.jsx
+++ nc-news/src/Components/CommentList.jsx
@@ -1,27 +1,8 @@
-import { useState, useEffect } from 'react'
-import { fetchCommentsByArticleId, deleteComment } from '../Utils/api'
+import React from 'react'
 import CommentCard from './CommentCard'
 
-const CommentList = ({ article_id }) => {
-  const [comments, setComments] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
-
-  useEffect(() => {
-    setLoading(true)
-    fetchCommentsByArticleId(article_id)
-      .then((response) => {
-        setComments(response.data)
-        setLoading(false)
-      })
-      .catch((err) => {
-        setError(err.message)
-        setLoading(false)
-      })
-  }, [article_id])
-
-  if (loading) return <p>Loading comments...</p>
-  if (error) return <p>Error loading comments: {error}</p>
+const CommentList = ({ comments }) => {
+  console.log(comments)
   if (comments.length === 0) return <p>No comments yet</p>
 
   return (
@@ -32,4 +13,5 @@ const CommentList = ({ article_id }) => {
     </div>
   )
 }
+
 export default CommentList
