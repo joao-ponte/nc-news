8153356c7bf90bcbc8d5b3c5464a426a0cf5eaff
Joao Ponte <joao_ponte@msn.com>
2024-06-20 09:51:44 +0100

    Passing props to CommentCard

--- nc-news/src/Components/CommentList.jsx
+++ nc-news/src/Components/CommentList.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import CommentCard from './CommentCard'
 
-const CommentList = ({ comments }) => {
+const CommentList = ({ comments, onDelete }) => {
   if (comments.length === 0) return <p>No comments yet</p>
 
   return (
     <div className="comment-list">
       {comments.map((comment) => (
-        <CommentCard key={comment.comment_id} comment={comment} />
+        <CommentCard
+          key={comment.comment_id}
+          comment={comment}
+          onDelete={onDelete}
+        />
       ))}
     </div>
   )
