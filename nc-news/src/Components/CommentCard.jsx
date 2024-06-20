fab2f52e65b0bcaa022b0f62f005a55bc1a0af15
Joao Ponte <joao_ponte@msn.com>
2024-06-20 09:50:32 +0100

    Logic to handleDelete.

--- nc-news/src/Components/CommentCard.jsx
+++ nc-news/src/Components/CommentCard.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { useState } from 'react'
+import { deleteComment } from '../Utils/api'
+import { useContext } from 'react'
+import { UserContext } from '../context/UserContext'
 
 const CommentCard = ({ comment, onDelete }) => {
-  const { author, body, votes } = comment
+  const { author, body, votes, comment_id } = comment
+  const { user } = useContext(UserContext)
+  const [deleting, setDeleting] = useState(false)
+
+  const handleDelete = () => {
+    setDeleting(true)
+    deleteComment(comment_id)
+      .then(() => {
+        console.log(`Successfully deleted comment with ID: ${comment_id}`)
+        onDelete(comment_id)
+      })
+      .catch((err) => {
+        console.error(
+          `Error deleting comment with ID: ${comment_id}`,
+          err.message
+        )
+        setDeleting(false)
+      })
+  }
 
   return (
     <div className="comment-card">
@@ -11,7 +32,11 @@ const CommentCard = ({ comment, onDelete }) => {
       <p>{body}</p>
       <div>
         <button>({votes})</button>
-        <button>Delete</button>
+        {user && user.username === author && (
+          <button onClick={handleDelete} disabled={deleting}>
+            {deleting ? 'Deleting...' : 'Delete'}
+          </button>
+        )}
       </div>
     </div>
   )
